11d1cb55f3c3ed90c49c25adac80736f9afdd775
Jorge.Balladares <jebq0001@red.ujaen.es>
2024-02-12 14:14:11 +0100

    refactor impl service

--- tarea1/src/main/java/com/price/application/services/AlarmServiceImpl.java
+++ tarea1/src/main/java/com/price/application/services/AlarmServiceImpl.java
@@ -23,10 +23,7 @@ public class AlarmServiceImpl implements AlarmService {
     private final AlarmRepo alarmRepo;
     private final ModelMapper modelMapper;
     private final Alarm_typeRepo alarmTypeRepo;
-    private static final String key_ = "xkey";
-    private static final String idTunnel_ = "idTunel";
-    private static final String alarmDate_ = "alarmDate";
-    private static final String severity_ = "severity";
+
 
     public AlarmServiceImpl(AlarmRepo alarmRepo, ModelMapper modelMapper, Alarm_typeRepo alarmTypeRepo) {
         this.alarmRepo = alarmRepo;
@@ -53,112 +50,15 @@ public class AlarmServiceImpl implements AlarmService {
     }
 
     public List<AlarmOutputVO> getListAlarmsByParams(String key, String idTunnel, String dateInit, String dateFinal, String severity) throws Exception {
-        String pointError = "";
-
+        if (Objects.nonNull(key)){
+            key = this.alarmTypeRepo.getIdByKey(key.toUpperCase()).toString();
+        }
         try {
-            boolean dates = Objects.nonNull(dateInit) && Objects.nonNull(dateFinal);
-            if (Objects.nonNull(key)) {
-                if (Objects.nonNull(idTunnel)) {
-                    if (dates) {
-                        if (Objects.nonNull(severity)) {
-                            pointError = key_ + ", " + idTunnel_ + ", " + alarmDate_ + ", " + severity_;
-                            log.debug("getting list from BD");
-                            int idKey = alarmTypeRepo.getIdByKey(key.toUpperCase());
-                            return alarmRepo.listAlarmByAllParams(Integer.parseInt(idTunnel), dateInit, dateFinal, String.valueOf(idKey), Integer.parseInt(severity)).stream().map(alarm -> modelMapper.map(alarm, AlarmOutputVO.class)).collect(Collectors.toList());
-                        } else {
-                            pointError = key_ + ", " + idTunnel_ + ", " + alarmDate_;
-                            log.debug("getting list from BD");
-                            int idKey = alarmTypeRepo.getIdByKey(key.toUpperCase());
-                            return alarmRepo.listAlarmByKeyTunnelDate(Integer.parseInt(idTunnel), dateInit, dateFinal, String.valueOf(idKey)).stream().map(alarm -> modelMapper.map(alarm, AlarmOutputVO.class)).collect(Collectors.toList());
-                        }
-                    } else {
-                        if (Objects.nonNull(severity)) {
-                            pointError = key_ + ", " + idTunnel_ + ", " + severity_;
-                            log.debug("getting list from BD");
-                            int idKey = alarmTypeRepo.getIdByKey(key.toUpperCase());
-                            return alarmRepo.listAlarmByKeyTunnelSev(Integer.parseInt(idTunnel), String.valueOf(idKey), Integer.parseInt(severity)).stream().map(alarm -> modelMapper.map(alarm, AlarmOutputVO.class)).collect(Collectors.toList());
-                        } else {
-                            pointError = key_ + " and " + idTunnel_;
-                            log.debug("getting list from BD");
-                            int idKey = alarmTypeRepo.getIdByKey(key.toUpperCase());
-                            return alarmRepo.listAlarmKeyTunnel(String.valueOf(idKey), idTunnel).stream().map(alarm -> modelMapper.map(alarm, AlarmOutputVO.class)).collect(Collectors.toList());
-                        }
-                    }
-                } else {
-                    if (dates) {
-                        if (Objects.nonNull(severity)) {
-                            pointError = key_ + ", " + alarmDate_ + ", " + severity_;
-                            log.debug("getting list from BD");
-                            int idKey = alarmTypeRepo.getIdByKey(key.toUpperCase());
-                            return alarmRepo.listAlarmByKeyDateSev(dateInit, dateFinal, String.valueOf(idKey), Integer.parseInt(severity)).stream().map(alarm -> modelMapper.map(alarm, AlarmOutputVO.class)).collect(Collectors.toList());
-                        } else {
-                            pointError = alarmDate_ + " and " + key_;
-                            log.debug("getting list from BD");
-                            int idKey = alarmTypeRepo.getIdByKey(key.toUpperCase());
-                            return alarmRepo.listAlarmKeyDates(String.valueOf(idKey), dateInit, dateFinal).stream().map(alarm -> modelMapper.map(alarm, AlarmOutputVO.class)).collect(Collectors.toList());
-                        }
-                    } else {
-                        if (Objects.nonNull(severity)) {
-                            pointError = key_ + " and " + severity_;
-                            log.debug("getting list from BD");
-                            int idKey = alarmTypeRepo.getIdByKey(key.toUpperCase());
-                            return alarmRepo.listAlarmBySevKey(Integer.parseInt(severity), String.valueOf(idKey)).stream().map(alarm -> modelMapper.map(alarm, AlarmOutputVO.class)).collect(Collectors.toList());
-                        } else {
-                            pointError = key_;
-                            log.debug("getting list from BD");
-                            int idKey = alarmTypeRepo.getIdByKey(key.toUpperCase());
-                            return alarmRepo.listAlarmByXkey(idKey).stream().map(alarm -> modelMapper.map(alarm, AlarmOutputVO.class)).collect(Collectors.toList());
-                        }
-                    }
-                }
-            }
-            else {
-                if (Objects.nonNull(idTunnel)) {
-                    if (dates) {
-                        if (Objects.nonNull(severity)) {
-                            pointError = idTunnel_ + ", " + alarmDate_ + ", " + severity_;
-                            log.debug("getting list from BD");
-                            return alarmRepo.listAlarmByTunnelDateSev(Integer.parseInt(idTunnel), dateInit, dateFinal, Integer.parseInt(severity)).stream().map(alarm -> modelMapper.map(alarm, AlarmOutputVO.class)).collect(Collectors.toList());
-                        } else {
-                            pointError = alarmDate_ + " and " + idTunnel_;
-                            log.debug("getting list from BD");
-                            return alarmRepo.listAlarmByIdTunnelDates(Integer.parseInt(idTunnel), dateInit, dateFinal).stream().map(alarm -> modelMapper.map(alarm, AlarmOutputVO.class)).collect(Collectors.toList());
-                        }
-                    } else {
-                        if (Objects.nonNull(severity)) {
-                            pointError = idTunnel_ + ", " + severity_;
-                            log.debug("getting list from BD");
-                            return alarmRepo.listAlarmByTunnelSev(Integer.parseInt(idTunnel), Integer.parseInt(severity)).stream().map(alarm -> modelMapper.map(alarm, AlarmOutputVO.class)).collect(Collectors.toList());
-                        } else {
-                            pointError = idTunnel_;
-                            log.debug("getting list from BD");
-                            return alarmRepo.listAlarmByIdTunnel(Integer.parseInt(idTunnel)).stream().map(alarm -> modelMapper.map(alarm, AlarmOutputVO.class)).collect(Collectors.toList());
-                        }
-                    }
-                } else {
-                    if (dates) {
-                        if (Objects.nonNull(severity)) {
-                            pointError = alarmDate_ + ", " + severity_;
-                            log.debug("getting list from BD");
-                            return alarmRepo.listAlarmByDateSev(dateInit, dateFinal, Integer.parseInt(severity)).stream().map(alarm -> modelMapper.map(alarm, AlarmOutputVO.class)).collect(Collectors.toList());
-                        } else {
-                            pointError = alarmDate_;
-                            log.debug("getting list from BD");
-                            return alarmRepo.listAlarmBetweenDates(dateInit, dateFinal).stream().map(alarm -> modelMapper.map(alarm, AlarmOutputVO.class)).collect(Collectors.toList());
-                        }
-                    } else {
-                        if (Objects.nonNull(severity)) {
-                            pointError = severity_;
-                            log.debug("getting list from BD");
-                            return alarmRepo.listAlarmBySev(Integer.parseInt(severity)).stream().map(alarm -> modelMapper.map(alarm, AlarmOutputVO.class)).collect(Collectors.toList());
-                        } else {
-                            return null;
-                        }
-                    }
-                }
-            }
+            log.debug("getting list from repository");
+            return this.alarmRepo.getList(idTunnel, dateInit, dateFinal, key, severity)
+                    .stream().map(alarm -> modelMapper.map(alarm, AlarmOutputVO.class)).collect(Collectors.toList());
         } catch (Exception e) {
-            throw new Exception("It was not possible to get a list with param: " + pointError);
+            throw new Exception("It was not possible to get a list with params");
 
         }
     }
--- tarea1/src/main/java/com/price/domain/repositories/AlarmRepo.java
+++ tarea1/src/main/java/com/price/domain/repositories/AlarmRepo.java
@@ -13,86 +13,18 @@ import java.util.List;
 @Component
 public interface AlarmRepo extends JpaRepository<Alarm, Integer>, JpaSpecificationExecutor<Alarm> {
 
-    @Query(value = "SELECT * from alarms where alarm_tpe = :idKey", nativeQuery = true)
-    List<Alarm> listAlarmByXkey(@Param("idKey") int idKey);
+    @Query(value = "SELECT * FROM ALARMS  " +
+            "WHERE (:idTunnel IS NULL OR idTunel = :idTunnel) " +
+            "AND (:idKey IS NULL OR alarm_tpe = :idKey) " +
+            "AND (:dateInit IS NULL OR :dateFinish IS NULL OR alarm " +
+            "BETWEEN PARSEDATETIME(:dateInit, 'yyyy-MM-dd.hh.mm.ss') " +
+            "AND PARSEDATETIME(:dateFinish,'yyyy-MM-dd.hh.mm.ss')) " +
+            "AND (:severity IS NULL OR severidad = :severity)", nativeQuery = true)
+    List<Alarm> getList(
+            @Param("idTunnel") String idTunnel, @Param("dateInit") String dateInit,
+            @Param("dateFinish") String dateFinish, @Param("idKey") String idKey,
+            @Param("severity") String severity
+    );
 
-    @Query(value = "SELECT * from alarms where IDTUNEL = :idTunnel", nativeQuery = true)
-    List<Alarm> listAlarmByIdTunnel(@Param("idTunnel") int idTunnel);
-
-    @Query(value = "SELECT * from alarms " +
-            "where alarm between PARSEDATETIME(:dateInit,'yyyy-MM-dd.hh.mm.ss') " +
-            "and PARSEDATETIME(:dateFinish, 'yyyy-MM-dd.hh.mm.ss')", nativeQuery = true)
-    List<Alarm> listAlarmBetweenDates(@Param("dateInit") String dateInit, @Param("dateFinish") String dateFinish);
-
-    @Query(value = "SELECT * from alarms where alarm_tpe = :idKey and idTunel = :tunnel ", nativeQuery = true)
-    List<Alarm> listAlarmKeyTunnel(@Param("idKey") String idKey, @Param("tunnel") String tunnel);
-
-    @Query(value = "SELECT * from alarms " +
-            "where alarm_tpe = :idKey " +
-            "and (alarm between PARSEDATETIME(:dateInit, 'yyyy-MM-dd.hh.mm.ss') " +
-            "and PARSEDATETIME(:dateFinish,'yyyy-MM-dd.hh.mm.ss'))", nativeQuery = true)
-    List<Alarm> listAlarmKeyDates(@Param("idKey") String idKey, @Param("dateInit") String dateInit, @Param("dateFinish") String dateFinish);
-
-    @Query(value = "SELECT * from alarms where IDTUNEL = :idTunnel " +
-            "and (alarm between PARSEDATETIME(:dateInit, 'yyyy-MM-dd.hh.mm.ss') " +
-            "and PARSEDATETIME(:dateFinish,'yyyy-MM-dd.hh.mm.ss'))", nativeQuery = true)
-    List<Alarm> listAlarmByIdTunnelDates(@Param("idTunnel") int idTunnel, @Param("dateInit") String dateInit, @Param("dateFinish") String dateFinish);
-
-    @Query(value = "SELECT * from alarms where severidad = :severity", nativeQuery = true)
-    List<Alarm> listAlarmBySev(@Param("severity") int severity);
-
-    @Query(value = "SELECT * from alarms where severidad = :severity " +
-            "and alarm_tpe = :idKey", nativeQuery = true)
-    List<Alarm> listAlarmBySevKey(@Param("severity") int severity, @Param("idKey") String idKey);
-
-    @Query(value = "SELECT * from alarms where IDTUNEL = :idTunnel " +
-            "and (alarm between PARSEDATETIME(:dateInit, 'yyyy-MM-dd.hh.mm.ss') " +
-            "and PARSEDATETIME(:dateFinish,'yyyy-MM-dd.hh.mm.ss')) " +
-            "and alarm_tpe = :idKey " +
-            "and severidad = :severity", nativeQuery = true)
-    List<Alarm> listAlarmByAllParams(@Param("idTunnel") int idTunnel, @Param("dateInit") String dateInit,
-                                     @Param("dateFinish") String dateFinish, @Param("idKey") String idKey,
-                                     @Param("severity") int severity);
-
-    @Query(value = "SELECT * from alarms where IDTUNEL = :idTunnel " +
-            "and (alarm between PARSEDATETIME(:dateInit, 'yyyy-MM-dd.hh.mm.ss') " +
-            "and PARSEDATETIME(:dateFinish,'yyyy-MM-dd.hh.mm.ss')) " +
-            "and alarm_tpe = :idKey ", nativeQuery = true)
-    List<Alarm> listAlarmByKeyTunnelDate(@Param("idTunnel") int idTunnel, @Param("dateInit") String dateInit,
-                                         @Param("dateFinish") String dateFinish, @Param("idKey") String idKey);
-
-    @Query(value = "SELECT * from alarms where IDTUNEL = :idTunnel " +
-            "and alarm_tpe = :idKey " +
-            "and severidad = :severity", nativeQuery = true)
-    List<Alarm> listAlarmByKeyTunnelSev(@Param("idTunnel") int idTunnel, @Param("idKey") String idKey,
-                                        @Param("severity") int severity);
-
-    @Query(value = "SELECT * from alarms where (alarm between PARSEDATETIME(:dateInit, 'yyyy-MM-dd.hh.mm.ss') " +
-            "and PARSEDATETIME(:dateFinish,'yyyy-MM-dd.hh.mm.ss')) " +
-            "and alarm_tpe = :idKey " +
-            "and severidad = :severity", nativeQuery = true)
-    List<Alarm> listAlarmByKeyDateSev(@Param("dateInit") String dateInit,
-                                      @Param("dateFinish") String dateFinish, @Param("idKey") String idKey,
-                                      @Param("severity") int severity);
-
-    @Query(value = "SELECT * from alarms where IDTUNEL = :idTunnel " +
-            "and (alarm between PARSEDATETIME(:dateInit, 'yyyy-MM-dd.hh.mm.ss') " +
-            "and PARSEDATETIME(:dateFinish,'yyyy-MM-dd.hh.mm.ss')) " +
-            "and severidad = :severity", nativeQuery = true)
-    List<Alarm> listAlarmByTunnelDateSev(@Param("idTunnel") int idTunnel, @Param("dateInit") String dateInit,
-                                     @Param("dateFinish") String dateFinish,
-                                     @Param("severity") int severity);
-
-    @Query(value = "SELECT * from alarms where IDTUNEL = :idTunnel " +
-            "and severidad = :severity", nativeQuery = true)
-    List<Alarm> listAlarmByTunnelSev(@Param("idTunnel") int idTunnel,
-                                         @Param("severity") int severity);
-
-    @Query(value = "SELECT * from alarms where (alarm between PARSEDATETIME(:dateInit, 'yyyy-MM-dd.hh.mm.ss') " +
-            "and PARSEDATETIME(:dateFinish,'yyyy-MM-dd.hh.mm.ss')) " +
-            "and severidad = :severity", nativeQuery = true)
-    List<Alarm> listAlarmByDateSev(@Param("dateInit") String dateInit,
-                                         @Param("dateFinish") String dateFinish,
-                                         @Param("severity") int severity);
 
 }
